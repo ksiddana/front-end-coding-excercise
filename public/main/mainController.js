d14efe10e555a390b67772431350d234e2b0dff9
Karun Siddana <ksiddana@gmail.com>
2016-05-17 11:52:39 -0700

    Adding comments to the scope.loadData function

--- public/main/mainController.js
+++ public/main/mainController.js
@@ -2,33 +2,35 @@ angular.module('truStar.main', ['MainServices'])
 
 .controller('MainController', ['$scope', 'MainFactory', 
   function($scope, MainFactory) {
-    
+  
+    //Retrieve all the data using the factory function and 
+    // store it on the scope variable called fakeData.
     $scope.loadData = function() {
       MainFactory.getAllData().then(function(data){
         $scope.fakeData = data;
       });
     }
 
-  // Setup up initial values for selecting reports
-  $scope.selectedIndex = null;
-  $scope.selectedData = null;
+    // Setup up initial values for selecting reports
+    $scope.selectedIndex = null;
+    $scope.selectedData = null;
 
-  // define a function that takes in the index of the cell that is selected 
-  // in the ng-repeat.
-  $scope.selectData = function (data, index) {
-    $scope.selectedIndex = index;
-    $scope.selectedData = data;
-  };
+    // define a function that takes in the index of the cell that is selected 
+    // in the ng-repeat.
+    $scope.selectData = function (data, index) {
+      $scope.selectedIndex = index;
+      $scope.selectedData = data;
+    };
 
-  // Custom Filter for filtering reports based on title
-  $scope.titleFilter = function(report) {
-    if ($scope.search) {
-      return report.title.indexOf($scope.search) !== -1
+    // Custom Filter for filtering reports based on title
+    $scope.titleFilter = function(report) {
+      if ($scope.search) {
+        return report.title.indexOf($scope.search) !== -1
+      }
+      return true;
     }
-    return true;
-  }
 
-  // Load the data without hiting the Refresh Button
-  $scope.loadData();
+    // Load the data without hiting the Refresh Button
+    $scope.loadData();
 
 }])
