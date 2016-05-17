75449433429b601b5aec472bf510dbc5cc3279df
Karun Siddana <ksiddana@gmail.com>
2016-05-17 11:46:36 -0700

    Added Custom Filter for title

--- public/main/mainController.js
+++ public/main/mainController.js
@@ -9,13 +9,24 @@ angular.module('truStar.main', ['MainServices'])
       });
     }
 
+  // Setup up initial values for selecting reports
   $scope.selectedIndex = null;
   $scope.selectedData = null;
 
+  // define a function that takes in the index of the cell that is selected 
+  // in the ng-repeat.
   $scope.selectData = function (data, index) {
     $scope.selectedIndex = index;
     $scope.selectedData = data;
   };
+
+  // Custom Filter for filtering reports based on title
+  $scope.titleFilter = function(report) {
+    if ($scope.search) {
+      return report.title.indexOf($scope.search) !== -1
+    }
+    return true;
+  }
 
   // Load the data without hiting the Refresh Button
   $scope.loadData();
